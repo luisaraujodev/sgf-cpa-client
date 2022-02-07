52832f448cadfa1513e24a013a23fab5aeb9735a
luisaraujodev <luisjtwo@gmail.com>
2022-02-06 23:12:14 -0300

    clear comments, add action windows & menu

--- src/background.ts
+++ src/background.ts
@@ -1,72 +1,69 @@
-'use strict'
-
-import { app, protocol, BrowserWindow } from 'electron'
+import { app, protocol, BrowserWindow, Menu, ipcMain, remote } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
-// Scheme must be registered before the app is ready
+import MenuTemplate from "../src/data/GetMenuTemplate";
+import path from "path";
+
+let win: BrowserWindow;
+let child: BrowserWindow;
 protocol.registerSchemesAsPrivileged([
   { scheme: 'app', privileges: { secure: true, standard: true } }
 ])
+//menu.popup(BrowserWindow.fromWebContents(event.sender))
 
-async function createWindow () {
-  // Create the browser window.
-  const win = new BrowserWindow({
+
+async function createWindow() {
+  win = new BrowserWindow({
     width: 800,
     height: 600,
+    titleBarStyle: 'hiddenInset',
+    frame: false,
     webPreferences: {
-
-      // Use pluginOptions.nodeIntegration, leave this alone
-      // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: (process.env
-        .ELECTRON_NODE_INTEGRATION as unknown) as boolean,
-      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js")
     }
   })
 
+  try {
+    win.setMenu(Menu.buildFromTemplate(MenuTemplate))
+  } catch (error) {
+    console.log("erro no menu")
+  }
+
   if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL as string)
     if (!process.env.IS_TEST) win.webContents.openDevTools()
   } else {
     createProtocol('app')
-    // Load the index.html when not in development
     win.loadURL('app://./index.html')
   }
 }
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
   if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
     try {
       await installExtension(VUEJS3_DEVTOOLS)
-    } catch (e:any) {
-      console.error('Vue Devtools failed to install:', e.toString())
+    } catch (e) {
+      if (e instanceof TypeError) {
+        console.error('Vue Devtools failed to install:', e.toString())
+      }
     }
   }
   createWindow()
 })
 
-// Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === 'win32') {
     process.on('message', (data) => {
@@ -80,3 +77,40 @@ if (isDevelopment) {
     })
   }
 }
+
+//Action Winodows
+
+ipcMain.on("action-menu", function (event, action): void {
+  switch (action) {
+    case 'new Windows':
+      child = new BrowserWindow({ parent: win });
+      child.setMenu(Menu.buildFromTemplate(MenuTemplate))
+      child.show();
+      break;
+    case 'appearance-light':
+      win.webContents.executeJavaScript("document.querySelector('html').setAttribute('theme', 'light')")
+      break;
+    case 'appearance-dark':
+      win.webContents.executeJavaScript("document.querySelector('html').setAttribute('theme', 'dark')")
+      break;
+  }
+})
+
+ipcMain.on("action-window", function (event, action): void {
+  const currentWindow: any = win || remote.BrowserWindow.getFocusedWindow()
+  switch (action) {
+    case 'minimize':
+      currentWindow.minimize();
+      break;
+    case 'maximize':
+      if (!currentWindow.isMaximized()) {
+        currentWindow.maximize();
+      } else {
+        currentWindow.unmaximize();
+      }
+      break;
+    case 'close':
+      currentWindow.close();
+      break;
+  }
+})
