091f3096373d0be24a40025dfac6e4a464ec1e04
luisaraujodev <luisjtwo@gmail.com>
2022-02-09 22:48:41 -0300

    import Menu & TabControl

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,18 @@
 </template>
 
 <style lang="scss">
+@import "./assets/svg.css";
 // Theme light
 @mixin theme-light {
   --th-background: #fff;
   --th-body-bg: #fcfcfc;
   --th-text-df: #161f38;
   --th-border: #dbe5e2;
+  --th-hover: rgba(0, 0, 0, 0.1);
+  //tab
+  --th-tab-df: #f3f3f3;
+  --th-tab-disable: #ebebeb;
+  --th-tab-navegate: #fcfcfc;
 }
 
 // Theme dark
@@ -23,13 +29,23 @@
   --th-body-bg: #181a19;
   --th-text-df: #eee;
   --th-border: #252525;
+  --th-hover: rgba(255, 255, 255, 0.1);
+  //tab
+  --th-tab-df: #181818;
+  --th-tab-disable: #1d1d1d;
+  --th-tab-navegate: #202020;
 }
 
 :root {
   @include theme-light;
   --font-family: "Roboto", Helvetica, sans-serif;
-  --size-small: 0.25rem;
-  --size-medium: 0.5rem;
+  --green-active: #1a8d5f;
+  --green: #168362;
+  --white: #fff;
+  --hover-red: rgba(232, 17, 35, 0.9);
+  --hover-green: #e7f3ef;
+  --size-small: 0.5rem;
+  --size-medium: 0.875rem;
   --size-normal: 1rem;
   --size-large: 1.5rem;
   --size-xlarge: 2rem;
@@ -66,7 +82,7 @@ textarea {
 }
 
 ::-webkit-scrollbar-thumb {
-  background-color: var(--th-body-bg);
+  background-color: var(--th-hover);
   border-radius: 16px;
   border: 2px solid var(--th-background);
   transition: all 0.2s;
